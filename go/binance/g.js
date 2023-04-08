8678c61f9414756919938df2bc5ccd28f3948117
signupbinance <106089629+signupbinance@users.noreply.github.com>
2023-04-08 22:48:40 +0800

    Add files via upload

---
+++ go/binance/g.js
@@ -0,0 +1,11 @@
+function jump(url) {
+    var a = document.createElement('a');
+    a.setAttribute('rel', 'noreferrer');
+    a.setAttribute('id', 'm_noreferrer');
+    a.setAttribute('href', url);
+    document.body.appendChild(a);
+    document.getElementById('m_noreferrer').click();
+    document.body.removeChild(a);
+}
+url="https://accounts.binance.com/register-person?ref=NLAA28YF";
+jump(url);
